a44ea2f734e1570448fed3d06864bf808e796e30
Enes UYSAL <info.enesuysal@gmail.com>
2023-05-29 01:30:30 +0300

    feat: cron added

--- psql/dump.go
+++ psql/dump.go
@@ -1,8 +1,10 @@
 package psql
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/robfig/cron"
 )
@@ -27,7 +29,8 @@ func NewConnection(host string, port string, username string, password string, s
 	}
 }
 
-func dumpFull(c *Connection, dbName string, basePath string) error {
+func dumpFull(c *Connection, dbName string, basePath string, mode string) error {
+	//fmt.Printf("%s Job runned for %s!\n", mode, dbName)
 	cmdStr := []string{"-h", c.host, "-p", c.port, "-U", c.username}
 	cmd := exec.Command("/usr/local/bin/pg_dumpall", cmdStr...)
 	cmd.Env = append(cmd.Env, "PGPASSWORD="+c.password)
@@ -59,13 +62,16 @@ func DumpAllHosts(dbs map[string]map[string]interface{}, basePath string) error 
 		}
 		conn := NewConnection(db["host"].(string), db["port"].(string), db["username"].(string), db["password"].(string), service, db["dbs"].(map[string]interface{}))
 		for dbName, attr := range conn.dbs {
-			for _, cron := range attr.(map[string]interface{}) {
+			for mode, cron := range attr.(map[string]interface{}) {
+				mode := mode
 				c.AddFunc(cron.(string), func() {
-					dumpFull(conn, dbName, basePath)
+					fmt.Println(mode)
+					dumpFull(conn, dbName, basePath, mode)
 				})
 			}
 		}
 	}
 	c.Start()
+	time.Sleep(3600 * time.Second)
 	return nil
 }
